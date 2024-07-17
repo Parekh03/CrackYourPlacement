4b1af55c027fe70e5806042f08e5a84fd38fe289
PAREKH VED <110220047+Parekh03@users.noreply.github.com>
2024-07-17 11:11:10 +0530

    Update 26_remove_duplicates_from_sorted_array.java

--- Day-01/26_remove_duplicates_from_sorted_array.java
+++ Day-01/26_remove_duplicates_from_sorted_array.java
@@ -1,1 +1,19 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int count=1;
 
+        int i=0, j=1;
+        while(j<nums.length){
+            if(nums[i]!=nums[j]){
+                i++;
+                count++;
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+            j++;
+        }
+
+        return count;
+    }
+}
