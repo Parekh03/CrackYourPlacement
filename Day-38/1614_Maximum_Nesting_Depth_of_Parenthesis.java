d170b110c0cb3ff88333902f25c738ed444ea3ae
PAREKH VED <110220047+Parekh03@users.noreply.github.com>
2024-08-23 10:11:25 +0530

    Create 1614_Maximum_Nesting_Depth_of_Parenthesis.java

---
+++ Day-38/1614_Maximum_Nesting_Depth_of_Parenthesis.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxDepth(String s) {
+        int count = 0;
+        int maxcount = 0;
+
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+
+            if(ch=='('){
+                count++;
+                maxcount = Math.max(maxcount, count);
+            }
+
+            else if(ch==')'){
+                count--;
+            }
+        }
+
+        return maxcount;
+    }
+}
