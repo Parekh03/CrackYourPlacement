622d8d5a5542610294a07cc65afc24f3bc30c1ce
PAREKH VED <110220047+Parekh03@users.noreply.github.com>
2024-08-27 20:22:16 +0530

    Update Nth_Root_of_Number.java

--- Day-42/Nth_Root_of_Number.java
+++ Day-42/Nth_Root_of_Number.java
@@ -3,8 +3,12 @@ class Solution
     public int pow(int x, int n){
         int ans = 1;
         
-        for(int i=1; i<=n; i++){
-            ans = ans*x;
+        for(int i = 1; i <= n; i++){
+            if(ans > Integer.MAX_VALUE / x) {
+                // If multiplication will cause overflow, return Integer.MAX_VALUE as a signal.
+                return Integer.MAX_VALUE;
+            }
+            ans = ans * x;
         }
         
         return ans;
@@ -12,28 +16,26 @@ class Solution
     
     public int NthRoot(int n, int m)
     {
-        // code here
         int low = 1;
         int high = m;
-        int ans = 1;
+        int ans = -1;
         
-        while(low<=high){
-            int mid = low + (high-low)/2;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
             
             int midPowN = pow(mid, n);
             
-            if(midPowN <= m){
-                ans = mid;
+            if(midPowN == m) {
+                return mid;  // Exact root found
+            }
+            else if(midPowN < m){
                 low = mid + 1;
             }
-            
             else{
                 high = mid - 1;
             }
-            
         }
         
         return ans;
-        
     }
 }
